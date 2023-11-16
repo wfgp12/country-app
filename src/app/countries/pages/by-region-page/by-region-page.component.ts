b22a1799de9998ddb7f37a336fec04d46527f7bb
wfgp12 <w.gutierrez1206@gmail.com>
2023-11-16 18:12:49 -0500

    refactor by region page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-bt-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,12 +12,16 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  public selectedRegion?: Region;
   public isLoading: boolean = false;
 
-  constructor(private countriesService: CountriesService){}
+  constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(term:string) {
+  searchByRegion(term: Region) {
     this.isLoading = true
+    this.selectedRegion = term;
+
     this.countriesService.searchRegion(term)
       .subscribe(countries => {
         this.countries = countries
